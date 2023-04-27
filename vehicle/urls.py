8794378abda744cd15f3ebe2fdaf4b1fc06960e2
Okumu Justine <okumujustine01@gmail.com>
2023-04-27 20:11:10 +0300

    update vehicle

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from vehicle.views import vehicle_list, delete_vehicle
+from vehicle.views import vehicle_list, delete_vehicle, add_vehicle, update_vehicle
 
 app_name = "vehicle_app"
 
 urlpatterns = [
     path("list", vehicle_list, name="vehicle_list_url"),
-    path("delete_vehicle/<vehicle_id>", delete_vehicle, name="delete_vehicle_url")
+    path("add", add_vehicle, name="add_vehicle_url"),
+    path("update/<vehicle_id>", update_vehicle, name="update_vehicle_url"),
+    path("delete/<vehicle_id>", delete_vehicle, name="delete_vehicle_url")
 ]
--- vehicle/views.py
+++ vehicle/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from vehicle.models import Vehicle
 from vehicle.forms import VehicleForm
@@ -9,13 +10,7 @@ from vehicle.forms import VehicleForm
 def vehicle_list(request):
     all_vehicles = Vehicle.objects.all()
 
-    # perform vehicle creation
-    if request.method == 'POST':
-        form_to_save = VehicleForm(request.POST)
-        if form_to_save.is_valid():
-            form_to_save.save()
-
-    context = {"vehicles": all_vehicles, "creat_vehicle_form": VehicleForm()}
+    context = {"vehicles": all_vehicles}
     return render(request, "vehicle/vehicle_list.html", context)
 
 
@@ -24,3 +19,25 @@ def delete_vehicle(request, vehicle_id):
     vehicle_to_delete = Vehicle.objects.get(pk=vehicle_id)
     vehicle_to_delete.delete()
     return redirect("vehicle_app:vehicle_list_url")
+
+
+def add_vehicle(request):
+    # perform vehicle creation
+    if request.method == 'POST':
+        form_to_save = VehicleForm(request.POST)
+        if form_to_save.is_valid():
+            form_to_save.save()
+    context = {"creat_vehicle_form": VehicleForm()}
+    return render(request, "vehicle/add_vehicle.html", context)
+
+
+def update_vehicle(request, vehicle_id):
+    vehicle_to_update = Vehicle.objects.get(pk=vehicle_id)
+    form = VehicleForm(request.POST or None, instance=vehicle_to_update)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Vehicle successfully updated")
+
+    context = {"vehicle": vehicle_to_update, "vehicle_form": form}
+    return render(request, "vehicle/update_vehicle.html", context)
