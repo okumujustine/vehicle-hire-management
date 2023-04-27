15e16a93cb609d0b3b7020a3b22310da73ce7159
Okumu Justine <okumujustine01@gmail.com>
2023-04-27 20:24:25 +0300

    add customer model

--- customer/models.py
+++ customer/models.py
@@ -1,1 +1,18 @@
-# Create your models here.
+from django.db import models
+
+
+class CustomerTypes(models.TextChoices):
+    PERSONAL = "Personal"
+    ORGANIZATION = "Organiszation"
+    GROUP = "Group"
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField(max_length=50, null=False, blank=False)
+    phone_option = models.CharField(max_length=50, null=True, blank=True)
+    type = models.CharField(max_length=50, choices=CustomerTypes.choices, blank=False, null=False)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
